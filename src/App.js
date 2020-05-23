0c5b0c754527e6be7fc7de91153beaf447b929e8
shunsuke sasaki <shun.sskd05@icloud.com>
2020-05-23 23:21:12 +0900

    key_test

--- src/App.js
+++ src/App.js
@@ -2,8 +2,20 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return <h1> Hello,world!</h1>;
+class App extends React.Component {
+  render() {
+    const users = [
+      { name: "Tanaka", age: 26 },
+      { name: "Suzuki", age: 32 },
+      { name: "Yamada", age: 43 }
+    ];
+    const userList = users.map((user, index) =>
+      <li key={index}>{user.name} (Age: {user.age})</li>
+    );
+    return (
+      <ul>{userList}</ul>
+    );
+  }
 }
 
 export default App;
